e02cb8849caa69f3fdb095d268602fc686495dfc
Wyatt Miller <wjmiller2016@gmail.com>
2016-12-30 17:52:25 -0500

    Begun TDD

---
+++ app/controllers/wyatt_controller_controller.rb
@@ -0,0 +1,4 @@
+class WyattControllerController < ApplicationController
+  def index
+  end
+end
--- test/integration/wyatt_test.rb
+++ test/integration/wyatt_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WyattTest < ActionDispatch::IntegrationTest
   test "shows specific message when visiting '/wyatt'" do
-    get "/wyatt"
+    get "/wyatt_controller/index"
     assert_response :success
 
     assert_select "p", "Hi James!"
