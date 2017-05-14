b133563666b4c26c7f116377a99b45b14556da0d
Wyatt Miller <wjmiller2016@gmail.com>
2017-05-14 00:26:28 -0400

    Changed routes.rb file, made HTML layout for wyatt_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-	get 'wyatt_controller/index'
-	root 'wyatt_controller#index'
+	get '/wyatt', to: 'wyatt_controller#index'
+	get '/wyatt/about', to: 'wyatt_controller#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
