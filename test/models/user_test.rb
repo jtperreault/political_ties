f6f2d63efeb990d0c9073a388c7ff8701d5148ef
Wyatt Miller <wjmiller2016@gmail.com>
2018-03-04 02:59:42 -0500

    Updated User model, test and Gemfile
    Still have some more things to work on with the user but I implemented some regex and uniqueness in the User model and the test checks out just fine. Updated Gemfile for spring having a bug of having to stop spring when trying to use the rails console when you have already opened and closed it.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,5 +19,24 @@ class UserTest < ActiveSupport::TestCase
     @user.email = " "
     assert_not @user.valid?
   end
-  
+
+  test "name should not be too long" do
+    @user.name = "a" * 99
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
 end
