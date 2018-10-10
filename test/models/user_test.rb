6b7c1cfe9a4d2a3478566bff9bb6899abd8b97c4
Wyatt J. Miller <wjmiller2016@gmail.com>
2018-10-09 20:52:01 -0400

    Refined user model and user test
    I got the test working on the notion that the email has to be lowercase and added thaet to the user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,4 +39,11 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
