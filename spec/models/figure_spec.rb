7532f3999b4f960f7aab2c273779a13f3c97b174
James T. Perreault <jperreault@terakeet.com>
2020-06-27 21:55:46 -0400

    add figure spec

--- spec/models/figure_spec.rb
+++ spec/models/figure_spec.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe Figure, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a name' do
+    invalid_instance = described_class.new(first_name: nil)
+    expect(invalid_instance.invalid?).to be_truthy
+  end
 end
